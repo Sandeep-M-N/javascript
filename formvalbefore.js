00b063c40292f2a4a9ed0332579f2bd661e95b35
Sandeep MN <sandeep.mn>
2024-06-11 17:26:30 +0530

    modified

---
+++ formvalbefore.js
@@ -0,0 +1,26 @@
+function checkdata(){
+    let user_id=document.getElementById("uid").value;
+    if(user_id.trim()==""){
+        // window.alert("hi");
+        document.getElementById("uerror").innerHTML="user id is required";
+        document.getElementById("uerror").style.color="red";
+    }
+    else if(!user_id.trim().match('^[a-zA-Z]{3,20}$')){
+        document.getElementById("uerror").innerHTML="user id must be in proper format";
+        document.getElementById("uerror").style.color="red";
+    }
+    else{
+        document.getElementById("uerror").innerHTML=""
+    }
+}
+function checkpass(){
+    let user_pass=document.getElementById("upass").value;
+    if(user_pass.trim()==""){
+        document.getElementById("upassword").innerHTML="password is required";
+        document.getElementById("upassword").style.color="red";
+    }
+    else if(!user_pass.trim().match(`[a-zA-Z0-9!@#$%^&*]{6,16}$`)){
+        document.getElementById("upassword").innerHTML="password is not valid";
+        document.getElementById("upassword").style.color="red";
+    }
+}
